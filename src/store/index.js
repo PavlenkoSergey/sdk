ffbc345282c518101340f485a45dd7be05355bb3
Sergey Pavlenko <mail.sergey.pavlenko@gmail.com>
2019-10-24 18:35:38 +0300

    add saving state to localStorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate';
 
 Vue.use(Vuex);
 
@@ -66,5 +67,6 @@ export default new Vuex.Store({
 		}
 	},
 	actions: {},
-	modules: {}
+	modules: {},
+	plugins: [createPersistedState()],
 })
